eddaa1a5c56fb54cc98f29bd06ea39acc5c8ace6
Josh Rime <joshrime1990@icloud.com>
2024-09-21 20:00:12 -0500

    putting together darkmode and localstorage hooks

---
+++ frontend/hooks/userDarkMode.js
@@ -0,0 +1,8 @@
+import {useState} from 'react'
+import { useLocalStorage } from './useLocalStorage'
+
+
+export function useDarkMode() {
+    const [dark, setDark] = useLocalStorage(false)
+    return [dark, setDark]
+}
